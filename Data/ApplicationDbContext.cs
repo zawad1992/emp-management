7e981bc4fab9dcde9935608a30c4a99ac3971857
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-11-02 02:45:23 +0600

    [Feat] Application Db Context for Migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HRMWeb.Models;
+
+namespace HRMWeb.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Employee>()
+            .HasKey(e => e.EmployeeId);
+
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.LastName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.Division)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.Building)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.Title)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.Room)
+            .IsRequired()
+            .HasMaxLength(20);
+    }
+}
