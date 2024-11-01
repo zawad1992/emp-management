9c65290c6ebb6cfef1b3239b3ce3fcbf378463c6
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-11-02 02:44:13 +0600

    [Feat] Employees Views Add, Edit and Delete

---
+++ Views/Employees/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HRMWeb.Views.Employees
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
