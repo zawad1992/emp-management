376c53631a43b3394b98de9fdbc39ff438a55923
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-11-02 02:44:31 +0600

    [Feat] Employees Model

---
+++ Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRMWeb.Models;
+
+public class Employee
+{
+    public int EmployeeId { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(50)]
+    public string Division { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(50)]
+    public string Building { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(50)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(20)]
+    public string Room { get; set; } = string.Empty;
+}
